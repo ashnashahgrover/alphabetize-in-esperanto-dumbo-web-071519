7616b90d5e8b6b392d2a1f9fdd6a04fac5571e34
Ashna Shah-Grover <as19@williams.edu>
2019-05-23 07:38:17 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,8 +1,7 @@
-require 'pry'
 def alphabetize(arr)
   alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".chars.to_a
   arr.sort_by do |phrase| 
-    phrase.chars.delete(" ").map do |letter| 
+    phrase.chars.delete(" ").collect do |letter| 
       alphabet.index(letter)
     end 
   end 

2f6020ff16ca1500d65a2c10c7ca5540770330e0
Ashna Shah-Grover <as19@williams.edu>
2019-05-23 07:34:52 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,7 +1,10 @@
 require 'pry'
 def alphabetize(arr)
   alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".chars.to_a
-  arr.sort_by { |phrase| alphabet.index(phrase[i]) }
+  arr.sort_by |phrase| do 
+    phrase.chars.delete()
+  
+  { |phrase| alphabet.index(phrase[i]) }
 end
 
 alphabetize(["bonan matenon", "ĉu vi parolas esperanton", "mi amas vin", "pacon"])

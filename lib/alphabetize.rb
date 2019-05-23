df4919b58117765a191ad3d25ec7ac7b88a02f50
Ashna Shah-Grover <as19@williams.edu>
2019-05-23 07:25:50 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,7 +1,7 @@
 require 'pry'
 def alphabetize(arr)
   alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".chars.to_a
-  arr.sort_by { |word| alphabet.index(word[0]) }
+  arr.sort_by { |phrase| alphabet.index(phrase[0]) }
 end
 
 alphabetize(["bonan matenon", "ĉu vi parolas esperanton", "mi amas vin", "pacon"])

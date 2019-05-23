e83d6805580a83a02a223d2bc5e862794f02332f
Ashna Shah-Grover <as19@williams.edu>
2019-05-23 07:21:54 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,4 +1,4 @@
 def alphabetize(arr)
   ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".chars.to_a
-  arr.collect { |word| ESPERANTO_ALPHABET.index(word[0])
+  arr.collect { |word| ESPERANTO_ALPHABET.index(word[0]) }
 end

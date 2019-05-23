557ee06110abff7281a6ba483b18e3d66a8f7cbb
Ashna Shah-Grover <as19@williams.edu>
2019-05-23 07:24:23 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,7 +2,6 @@ require 'pry'
 def alphabetize(arr)
   alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".chars.to_a
   arr.sort_by { |word| alphabet.index(word[0]) }
-  binding.pry
 end
 
 alphabetize(["bonan matenon", "ĉu vi parolas esperanton", "mi amas vin", "pacon"])
